c59ff0820d1f6de6f2859d1f694554248238b8bc
andrewvk <avk@rsdn.ru>
2016-03-18 01:44:44 +0300

    EnumerableExtensions: Concat, Prepend

---
+++ Main/src/Collections/EnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace CodeJam
+{
+	/// <summary>
+	/// Extensions for <see cref="IEnumerable{T}"/>
+	/// </summary>
+	[PublicAPI]
+	public static class EnumerableExtensions
+	{
+		/// <summary>
+		/// Append specified <paramref name="element"/> to end of the collection.
+		/// </summary>
+		public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, T element)
+		{
+			foreach (var item in source)
+				yield return item;
+			yield return element;
+		}
+
+		/// <summary>
+		/// Prepend specified <paramref name="element"/> to the collection start.
+		/// </summary>
+		public static IEnumerable<T> Prepend<T>(this IEnumerable<T> source, T element)
+		{
+			yield return element;
+			foreach (var item in source)
+				yield return item;
+		}
+	}
+}
---
+++ Main/tests/Collections/EnumerableExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CodeJam
+{
+	[TestFixture]
+	public class EnumerableExtensionTests
+	{
+		[Test]
+		public void Concat()
+		{
+			Assert.AreEqual("1, 2, 3", new[] {"1", "2"}.Concat("3").Join(", "), "#A01");
+			Assert.AreEqual("3", new string[0].Concat("3").Join(", "), "#A02");
+		}
+
+		[Test]
+		public void Prepend()
+		{
+			Assert.AreEqual("0, 1, 2", new[] { "1", "2" }.Prepend("0").Join(", "), "#A01");
+			Assert.AreEqual("0", new string[0].Prepend("0").Join(", "), "#A02");
+		}
+	}
+}
