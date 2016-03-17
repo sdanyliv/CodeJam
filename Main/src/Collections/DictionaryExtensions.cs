310f9ce464387428093cefeafa1d241fb2394678
andrewvk <avk@rsdn.ru>
2016-03-18 02:12:07 +0300

    Dictionary.GetOrAdd

--- Main/src/Collections/DictionaryExtensions.cs
+++ Main/src/Collections/DictionaryExtensions.cs
@@ -11,6 +11,7 @@ namespace CodeJam
 	[PublicAPI]
 	public static class DictionaryExtensions
 	{
+		#region GetValueOrDefault
 		/// <summary>
 		/// Returns value associated with <paramref name="key"/>, or default(TValue) if key does not exists in
 		/// <paramref name="dictionary"/>
@@ -33,7 +34,7 @@ namespace CodeJam
 		public static TValue GetValueOrDefault<TKey, TValue>([NotNull] this Dictionary<TKey, TValue> dictionary, TKey key)
 		{
 			// Resolve ambiguity between IDictionary and IReadOnlyDictionary in System.Dictionary class.
-			return GetValueOrDefault((IDictionary<TKey, TValue>)dictionary, key);
+			return GetValueOrDefault((IDictionary<TKey, TValue>) dictionary, key);
 		}
 
 		/// <summary>
@@ -52,5 +53,41 @@ namespace CodeJam
 					? result
 					: default(TValue);
 		}
+		#endregion
+
+		#region GetOrAdd, AddOrUpdate
+		/// <summary>
+		/// Returns value by <paramref name="key"/>, or adds specified <paramref name="value"/> to dictionary.
+		/// </summary>
+		public static TValue GetOrAdd<TKey, TValue>(
+			[NotNull] this IDictionary<TKey, TValue> dictionary,
+			TKey key,
+			TValue value)
+		{
+			if (dictionary == null) throw new ArgumentNullException(nameof(dictionary));
+			TValue result;
+			if (dictionary.TryGetValue(key, out result))
+				return result;
+			dictionary.Add(key, value);
+			return value;
+		}
+
+		/// <summary>
+		/// Returns value by <paramref name="key"/>, or adds value created by <paramref name="valueFactory"/> to dictionary.
+		/// </summary>
+		public static TValue GetOrAdd<TKey, TValue>(
+			[NotNull] this IDictionary<TKey, TValue> dictionary,
+			TKey key,
+			Func<TKey, TValue> valueFactory)
+		{
+			if (dictionary == null) throw new ArgumentNullException(nameof(dictionary));
+			TValue result;
+			if (dictionary.TryGetValue(key, out result))
+				return result;
+			var value = valueFactory(key);
+			dictionary.Add(key, value);
+			return value;
+		}
+		#endregion
 	}
 }
---
+++ Main/tests/Collections/DictionaryExtensionsTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace CodeJam
+{
+	[TestFixture]
+	public class DictionaryExtensionsTest
+	{
+		[Test]
+		public static void GetOrAdd()
+		{
+			var dic = new Dictionary<int, string>();
+
+			var v1 = dic.GetOrAdd(1, "1");
+			Assert.AreEqual(1, dic.Count, "#A01");
+			Assert.AreEqual("1", dic[1], "#A02");
+			Assert.AreEqual("1", v1, "#A03");
+
+			var v2 = dic.GetOrAdd(1, "2");
+			Assert.AreEqual(1, dic.Count, "#A04");
+			Assert.AreEqual("1", dic[1], "#A05");
+			Assert.AreEqual("1", v2, "#A06");
+		}
+
+		[Test]
+		public static void GetOrAddLazy()
+		{
+			var dic = new Dictionary<int, string>();
+			var calls = 0;
+
+			var v1 = dic.GetOrAdd(1, k => { calls++; return "1"; });
+			Assert.AreEqual(1, dic.Count, "#A01");
+			Assert.AreEqual("1", dic[1], "#A02");
+			Assert.AreEqual("1", v1, "#A03");
+			Assert.AreEqual(1, calls, "#A07");
+
+			var v2 = dic.GetOrAdd(1, k => { calls++; return "2"; });
+			Assert.AreEqual(1, dic.Count, "#A04");
+			Assert.AreEqual("1", dic[1], "#A05");
+			Assert.AreEqual("1", v2, "#A06");
+			Assert.AreEqual(1, calls, "#A08");
+		}
+	}
+}
