49460f6bbddde91006b02b2e84295c709ef3cd9d
andrewvk <avk@rsdn.ru>
2016-03-18 14:22:29 +0300

    Array.Empty

---
+++ Main/src/Collections/Array.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace CodeJam
+{
+	[PublicAPI]
+	public static class Array<T>
+	{
+		/// <summary>
+		/// Empty instance of <typeparamref name="T"/>[].
+		/// </summary>
+		[NotNull]
+		public static readonly T[] Empty = new T[0];
+	}
+}
