13e48fe56c5a60d3879afcf198021c337829412c
andrewvk <avk@rsdn.ru>
2016-03-21 23:49:48 +0300

    Min()

---
+++ Main/src/Collections/EnumerableExtensions.Min.cs
@@ -0,0 +1,535 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace CodeJam
+{
+	partial class EnumerableExtensions
+	{
+		private static Exception NoElementsException() => new InvalidOperationException("Collection has no elements");
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+			[NotNull] this IEnumerable<TSource> source,
+			[NotNull, InstantHandle] Func<TSource, int> selector)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				if (!e.MoveNext())
+					throw NoElementsException();
+
+				var value = selector(e.Current);
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var x = selector(e.Current);
+					if (x < value)
+					{
+						value = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+			[NotNull] this IEnumerable<TSource> source,
+			[NotNull, InstantHandle] Func<TSource, int?> selector)
+			where TSource : class
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				int? value;
+				// Start off knowing that we've a non-null value (or exit here, knowing we don't)
+				// so we don't have to keep testing for nullity.
+				do
+				{
+					if (!e.MoveNext())
+						return default(TSource);
+
+					value = selector(e.Current);
+				}
+				while (!value.HasValue);
+
+				// Keep hold of the wrapped value, and do comparisons on that, rather than
+				// using the lifted operation each time.
+				var valueVal = value.GetValueOrDefault();
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var cur = selector(e.Current);
+					var x = cur.GetValueOrDefault();
+
+					// Do not replace & with &&. The branch prediction cost outweighs the extra operation
+					// unless nulls either never happen or always happen.
+					if (cur.HasValue & x < valueVal)
+					{
+						valueVal = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+			[NotNull] this IEnumerable<TSource> source,
+			[NotNull, InstantHandle] Func<TSource, long> selector)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				if (!e.MoveNext())
+					throw NoElementsException();
+
+				var value = selector(e.Current);
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var x = selector(e.Current);
+					if (x < value)
+					{
+						value = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+				[NotNull] this IEnumerable<TSource> source,
+				[NotNull, InstantHandle] Func<TSource, long?> selector)
+			where TSource : class
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				long? value;
+				// Start off knowing that we've a non-null value (or exit here, knowing we don't)
+				// so we don't have to keep testing for nullity.
+				do
+				{
+					if (!e.MoveNext())
+						return default(TSource);
+
+					value = selector(e.Current);
+				}
+				while (!value.HasValue);
+
+				// Keep hold of the wrapped value, and do comparisons on that, rather than
+				// using the lifted operation each time.
+				var valueVal = value.GetValueOrDefault();
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var cur = selector(e.Current);
+					var x = cur.GetValueOrDefault();
+
+					// Do not replace & with &&. The branch prediction cost outweighs the extra operation
+					// unless nulls either never happen or always happen.
+					if (cur.HasValue & x < valueVal)
+					{
+						valueVal = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+			[NotNull] this IEnumerable<TSource> source,
+			[NotNull, InstantHandle] Func<TSource, float> selector)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				if (!e.MoveNext())
+					throw NoElementsException();
+
+				var value = selector(e.Current);
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var x = selector(e.Current);
+					if (x < value)
+					{
+						value = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+				[NotNull] this IEnumerable<TSource> source,
+				[NotNull, InstantHandle] Func<TSource, float?> selector)
+			where TSource : class
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				float? value;
+				// Start off knowing that we've a non-null value (or exit here, knowing we don't)
+				// so we don't have to keep testing for nullity.
+				do
+				{
+					if (!e.MoveNext())
+						return default(TSource);
+
+					value = selector(e.Current);
+				}
+				while (!value.HasValue);
+
+				// Keep hold of the wrapped value, and do comparisons on that, rather than
+				// using the lifted operation each time.
+				var valueVal = value.GetValueOrDefault();
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var cur = selector(e.Current);
+					var x = cur.GetValueOrDefault();
+
+					// Do not replace & with &&. The branch prediction cost outweighs the extra operation
+					// unless nulls either never happen or always happen.
+					if (cur.HasValue & x < valueVal)
+					{
+						valueVal = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+			[NotNull] this IEnumerable<TSource> source,
+			[NotNull, InstantHandle] Func<TSource, double> selector)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				if (!e.MoveNext())
+					throw NoElementsException();
+
+				var value = selector(e.Current);
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var x = selector(e.Current);
+					if (x < value)
+					{
+						value = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+				[NotNull] this IEnumerable<TSource> source,
+				[NotNull, InstantHandle] Func<TSource, double?> selector)
+			where TSource : class
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				double? value;
+				// Start off knowing that we've a non-null value (or exit here, knowing we don't)
+				// so we don't have to keep testing for nullity.
+				do
+				{
+					if (!e.MoveNext())
+						return default(TSource);
+
+					value = selector(e.Current);
+				}
+				while (!value.HasValue);
+
+				// Keep hold of the wrapped value, and do comparisons on that, rather than
+				// using the lifted operation each time.
+				var valueVal = value.GetValueOrDefault();
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var cur = selector(e.Current);
+					var x = cur.GetValueOrDefault();
+
+					// Do not replace & with &&. The branch prediction cost outweighs the extra operation
+					// unless nulls either never happen or always happen.
+					if (cur.HasValue & x < valueVal)
+					{
+						valueVal = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+			[NotNull] this IEnumerable<TSource> source,
+			[NotNull, InstantHandle] Func<TSource, decimal> selector)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				if (!e.MoveNext())
+					throw NoElementsException();
+
+				var value = selector(e.Current);
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var x = selector(e.Current);
+					if (x < value)
+					{
+						value = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		[Pure]
+		public static TSource Min<TSource>(
+				[NotNull] this IEnumerable<TSource> source,
+				[NotNull, InstantHandle] Func<TSource, decimal?> selector)
+			where TSource : class
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+			TSource item;
+			using (var e = source.GetEnumerator())
+			{
+				decimal? value;
+				// Start off knowing that we've a non-null value (or exit here, knowing we don't)
+				// so we don't have to keep testing for nullity.
+				do
+				{
+					if (!e.MoveNext())
+						return default(TSource);
+
+					value = selector(e.Current);
+				}
+				while (!value.HasValue);
+
+				// Keep hold of the wrapped value, and do comparisons on that, rather than
+				// using the lifted operation each time.
+				var valueVal = value.GetValueOrDefault();
+				item = e.Current;
+				while (e.MoveNext())
+				{
+					var cur = selector(e.Current);
+					var x = cur.GetValueOrDefault();
+
+					// Do not replace & with &&. The branch prediction cost outweighs the extra operation
+					// unless nulls either never happen or always happen.
+					if (cur.HasValue & x < valueVal)
+					{
+						valueVal = x;
+						item = e.Current;
+					}
+				}
+			}
+
+			return item;
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="selector"/> on each element of a <paramref name="source"/>
+		/// and returns the item with minimum value.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <typeparam name="TValue">Type of the value</typeparam>
+		/// <param name="source">A sequence of values to determine the minimum value of.</param>
+		/// <param name="selector">A transform function to apply to each element.</param>
+		/// <returns>The item with minimum value in the sequence.</returns>
+		public static TSource Min<TSource, TValue>(
+			[NotNull] this IEnumerable<TSource> source,
+			[NotNull, InstantHandle] Func<TSource, TValue> selector)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+			var comparer = Comparer<TValue>.Default;
+			var value = default(TValue);
+			var item = default(TSource);
+			if (value == null)
+			{
+				using (var e = source.GetEnumerator())
+				{
+					do
+					{
+						if (!e.MoveNext())
+							return item;
+
+						value = selector(e.Current);
+						item = e.Current;
+					}
+					while (value == null);
+
+					while (e.MoveNext())
+					{
+						var x = selector(e.Current);
+						if (x != null && comparer.Compare(x, value) < 0)
+						{
+							value = x;
+							item = e.Current;
+						}
+					}
+				}
+			}
+			else
+			{
+				using (var e = source.GetEnumerator())
+				{
+					if (!e.MoveNext())
+						throw NoElementsException();
+
+					value = selector(e.Current);
+					item = e.Current;
+					while (e.MoveNext())
+					{
+						var x = selector(e.Current);
+						if (comparer.Compare(x, value) < 0)
+						{
+							value = x;
+							item = e.Current;
+						}
+					}
+				}
+			}
+
+			return item;
+		}
+	}
+}
---
+++ Main/tests/Collections/AggregateFuncsTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+namespace CodeJam
+{
+	[TestFixture]
+	public class AggregateFuncsTest
+	{
+		[TestCase(new[] {3, 1, 0, 4, 6}, ExpectedResult = 0)]
+		[TestCase(new[] { 1 }, ExpectedResult = 1)]
+		public int MinInt(int[] source) => source.Select(v => new Item<int>(v)).Min(i => i.Value).Value;
+
+		[TestCase(new[] {3, 1, 0, 4, 6}, ExpectedResult = 0)]
+		[TestCase(new[] {1}, ExpectedResult = 1)]
+		[TestCase(new int[0], ExpectedResult = null)]
+		public int? MinNullableInt(int[] source) => source.Select(v => new Item<int?>(v)).Min(i => i.Value)?.Value;
+
+		[TestCase(new[] { 3, 1, 0, 4, 6 }, ExpectedResult = 0)]
+		[TestCase(new[] { 1 }, ExpectedResult = 1)]
+		public float MinFloat(int[] source) => source.Select(v => new Item<float>(v)).Min(i => i.Value).Value;
+
+		[TestCase(new[] { 3, 1, 0, 4, 6 }, ExpectedResult = 0)]
+		[TestCase(new[] { 1 }, ExpectedResult = 1)]
+		[TestCase(new int[0], ExpectedResult = null)]
+		public float? MinNullableFloat(int[] source) => source.Select(v => new Item<float?>(v)).Min(i => i.Value)?.Value;
+
+		[TestCase(new[] { 3, 1, 0, 4, 6 }, ExpectedResult = 0)]
+		[TestCase(new[] { 1 }, ExpectedResult = 1)]
+		public double MinDouble(int[] source) => source.Select(v => new Item<double>(v)).Min(i => i.Value).Value;
+
+		[TestCase(new[] { 3, 1, 0, 4, 6 }, ExpectedResult = 0)]
+		[TestCase(new[] { 1 }, ExpectedResult = 1)]
+		[TestCase(new int[0], ExpectedResult = null)]
+		public double? MinNullableDouble(int[] source) => source.Select(v => new Item<double?>(v)).Min(i => i.Value)?.Value;
+
+		[TestCase(new[] { 3, 1, 0, 4, 6 }, ExpectedResult = 0)]
+		[TestCase(new[] { 1 }, ExpectedResult = 1)]
+		public decimal MinDec(int[] source) => source.Select(v => new Item<decimal>(v)).Min(i => i.Value).Value;
+
+		[TestCase(new[] { 3, 1, 0, 4, 6 }, ExpectedResult = 0)]
+		[TestCase(new[] { 1 }, ExpectedResult = 1)]
+		[TestCase(new int[0], ExpectedResult = null)]
+		public decimal? MinNullableDec(int[] source) => source.Select(v => new Item<decimal?>(v)).Min(i => i.Value)?.Value;
+
+		[TestCase(new[] { 3, 1, 0, 4, 6 }, ExpectedResult = "0")]
+		[TestCase(new[] { 1 }, ExpectedResult = "1")]
+		public string MinStr(int[] source) => source.Select(v => new Item<string>(v.ToString())).Min(i => i.Value).Value;
+
+		#region Item class
+		private class Item<T>
+		{
+			public Item(T value)
+			{
+				Value = value;
+			}
+
+			public T Value { get; }
+		}
+		#endregion
+	}
+}
