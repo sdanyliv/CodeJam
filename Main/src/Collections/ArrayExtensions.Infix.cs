d8d5f0c9081c94425fd441ef3bbb2e64231aac8d
andrewvk <avk@rsdn.ru>
2016-03-23 00:58:27 +0300

    Array.Equals template&generic

--- Main/src/Collections/ArrayExtensions.Infix.cs
+++ Main/src/Collections/ArrayExtensions.Infix.cs
@@ -6,11 +6,7 @@ using JetBrains.Annotations;
 
 namespace CodeJam.Collections
 {
-	/// <summary>
-	/// <see cref="Array" /> class extensions.
-	/// </summary>
-	[PublicAPI]
-	public static class ArrayExtensions
+	static partial class ArrayExtensions
 	{
 		/// <summary>Returns a read-only wrapper for the specified array.</summary>
 		/// <returns>A read-only <see cref="ReadOnlyCollection{T}" /> wrapper for the specified array.</returns>
---
+++ Main/src/Collections/ArrayExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.Collections
+{
+	/// <summary>
+	/// <see cref="Array" /> class extensions.
+	/// </summary>
+	[PublicAPI]
+	public partial class ArrayExtensions
+	{
+		/// <summary>
+		/// Returns true, if length and content of <paramref name="a"/> equals <paramref name="b"/>.
+		/// </summary>
+		[Pure]
+		public static bool Equals<T>([CanBeNull] this T[] a, [CanBeNull] T[] b, [NotNull] IEqualityComparer<T> comparer)
+		{
+			if (comparer == null) throw new ArgumentNullException(nameof(comparer));
+
+			if (a == null && b == null)
+				return true;
+
+			if (a == null || b == null)
+				return false;
+
+			if (a.Length != b.Length)
+				return false;
+
+			for (var i = 0; i < a.Length; i++)
+				if (!comparer.Equals(a[i], b[i]))
+					return false;
+			return true;
+		}
+
+		/// <summary>
+		/// Returns true, if length and content of <paramref name="a"/> equals <paramref name="b"/>.
+		/// </summary>
+		public static bool Equals<T>(this T[] a, T[] b) => Equals(a, b, EqualityComparer<T>.Default);
+
+		/// <summary>
+		/// Returns true, if length and content of <paramref name="a"/> equals <paramref name="b"/>.
+		/// </summary>
+		[Pure]
+		public static bool Equals([CanBeNull] this int[] a, [CanBeNull] int[] b)
+		{
+			if (a == null && b == null)
+				return true;
+
+			if (a == null || b == null)
+				return false;
+
+			if (a.Length != b.Length)
+				return false;
+
+			for (var i = 0; i < a.Length; i++)
+				if (a[i] == b[i])
+					return false;
+			return true;
+		}
+	}
+}
