e2f79288b9269bff59d8e3f15408d2465c0a1f5d
rameel <rameel-b@hotmail.com>
2016-03-24 18:04:23 +0600

    Added KeyEqualityComparer helper

---
+++ Main/src/Collections/KeyEqualityComparer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.Collections
+{
+	/// <summary>
+	/// Provides static methods for creating <see cref="KeyEqualityComparer{T,TKey}"/>.
+	/// </summary>
+	public static class KeyEqualityComparer
+	{
+		/// <summary>
+		/// Creates a <seealso cref="KeyEqualityComparer{T,TKey}"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of the comparing elements.</typeparam>
+		/// <typeparam name="TKey">The type of the key.</typeparam>
+		/// <param name="keySelector">The function to extract the key for each element.</param>
+		/// <returns>
+		/// A <seealso cref="KeyEqualityComparer{T,TKey}"/>.
+		/// </returns>
+		[NotNull]
+		public static KeyEqualityComparer<T, TKey> Create<T, TKey>([NotNull] Func<T, TKey> keySelector) =>
+			new KeyEqualityComparer<T, TKey>(keySelector);
+
+		/// <summary>
+		/// Creates a <seealso cref="KeyEqualityComparer{T,TKey}"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of the comparing elements.</typeparam>
+		/// <typeparam name="TKey">The type of the key.</typeparam>
+		/// <param name="keySelector">The function to extract the key for each element.</param>
+		/// <param name="comparer">The equality comparer to use to compare the keys.</param>
+		/// <returns>
+		/// A <seealso cref="KeyEqualityComparer{T,TKey}"/>.
+		/// </returns>
+		[NotNull]
+		public static KeyEqualityComparer<T, TKey> Create<T, TKey>(
+			[NotNull] Func<T, TKey> keySelector,
+			[CanBeNull] IEqualityComparer<TKey> comparer) =>
+				new KeyEqualityComparer<T, TKey>(keySelector, comparer);
+	}
+}
