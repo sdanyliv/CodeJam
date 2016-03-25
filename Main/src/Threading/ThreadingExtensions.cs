4747fc82997ad09cf87e6c19278cf87626d77a41
andrewvk <avk@rsdn.ru>
2016-03-26 02:41:57 +0300

    Memoize features, refactoring, performance and fixes

--- Main/src/Threading/ThreadingExtensions.cs
+++ Main/src/Threading/ThreadingExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using CodeJam.Collections;
+
 using JetBrains.Annotations;
 
 namespace CodeJam.Threading
@@ -9,7 +11,6 @@ namespace CodeJam.Threading
 	/// Threading related extension methods.
 	/// </summary>
 	[PublicAPI]
-	// TODO: replace Tuple class to struct for performance reasons
 	public static class ThreadingExtensions
 	{
 		#region Memoize
@@ -18,6 +19,7 @@ namespace CodeJam.Threading
 		/// </summary>
 		/// <param name="func">Function to memoize.</param>
 		/// <param name="comparer">Argument comparer</param>
+		/// <param name="threadSafe">If true - returns thread safe implementation</param>
 		/// <typeparam name="TArg">Type of argument</typeparam>
 		/// <typeparam name="TResult">Type of result</typeparam>
 		/// <returns>Memoized function</returns>
@@ -25,25 +27,30 @@ namespace CodeJam.Threading
 		[Pure]
 		public static Func<TArg, TResult> Memoize<TArg, TResult>(
 			[NotNull] this Func<TArg, TResult> func,
-			IEqualityComparer<TArg> comparer) =>
-				new ConcurrentLazyDictionary<TArg, TResult>(func, comparer).Get;
+			IEqualityComparer<TArg> comparer,
+			bool threadSafe = false) =>
+				LazyDictionary.Create(func, comparer, threadSafe).Get;
 
 		/// <summary>
 		/// Caches function value for specific argument.
 		/// </summary>
 		/// <param name="func">Function to memoize.</param>
+		/// <param name="threadSafe">If true - returns thread safe implementation</param>
 		/// <typeparam name="TArg">Type of argument</typeparam>
 		/// <typeparam name="TResult">Type of result</typeparam>
 		/// <returns>Memoized function</returns>
 		[NotNull]
 		[Pure]
-		public static Func<TArg, TResult> Memoize<TArg, TResult>([NotNull] this Func<TArg, TResult> func) =>
-			new ConcurrentLazyDictionary<TArg, TResult>(func).Get;
+		public static Func<TArg, TResult> Memoize<TArg, TResult>(
+				[NotNull] this Func<TArg, TResult> func,
+				bool threadSafe = false) =>
+			LazyDictionary.Create(func, threadSafe).Get;
 
 		/// <summary>
 		/// Caches function value for specific arguments.
 		/// </summary>
 		/// <param name="func">Function to memoize.</param>
+		/// <param name="threadSafe">If true - returns thread safe implementation</param>
 		/// <typeparam name="TArg1">Type of argument 1</typeparam>
 		/// <typeparam name="TArg2">Type of argument 2</typeparam>
 		/// <typeparam name="TResult">Type of result</typeparam>
@@ -51,14 +58,18 @@ namespace CodeJam.Threading
 		[NotNull]
 		[Pure]
 		public static Func<TArg1, TArg2, TResult> Memoize<TArg1, TArg2, TResult>(
-			[NotNull] this Func<TArg1, TArg2, TResult> func) =>
-				(arg1, arg2) => new ConcurrentLazyDictionary<Tuple<TArg1, TArg2>, TResult>(
-					key => func(key.Item1, key.Item2)).Get(Tuple.Create(arg1, arg2));
+			[NotNull] this Func<TArg1, TArg2, TResult> func,
+			bool threadSafe = false)
+		{
+			var map = LazyDictionary.Create<TupleStruct<TArg1, TArg2>, TResult>(key => func(key.Item1, key.Item2), threadSafe);
+			return (arg1, arg2) => map.Get(TupleStruct.Create(arg1, arg2));
+		}
 
 		/// <summary>
 		/// Caches function value for specific arguments.
 		/// </summary>
 		/// <param name="func">Function to memoize.</param>
+		/// <param name="threadSafe">If true - returns thread safe implementation</param>
 		/// <typeparam name="TArg1">Type of argument 1</typeparam>
 		/// <typeparam name="TArg2">Type of argument 2</typeparam>
 		/// <typeparam name="TArg3">Type of argument 3</typeparam>
@@ -67,14 +78,21 @@ namespace CodeJam.Threading
 		[NotNull]
 		[Pure]
 		public static Func<TArg1, TArg2, TArg3, TResult> Memoize<TArg1, TArg2, TArg3, TResult>(
-			[NotNull] this Func<TArg1, TArg2, TArg3, TResult> func) =>
-				(arg1, arg2, arg3) => new ConcurrentLazyDictionary<Tuple<TArg1, TArg2, TArg3>, TResult>(
-					key => func(key.Item1, key.Item2, key.Item3)).Get(Tuple.Create(arg1, arg2, arg3));
+			[NotNull] this Func<TArg1, TArg2, TArg3, TResult> func,
+			bool threadSafe = false)
+		{
+			var map =
+				LazyDictionary.Create<TupleStruct<TArg1, TArg2, TArg3>, TResult>(
+					key => func(key.Item1, key.Item2, key.Item3),
+					threadSafe);
+			return (arg1, arg2, arg3) => map.Get(TupleStruct.Create(arg1, arg2, arg3));
+		}
 
 		/// <summary>
 		/// Caches function value for specific arguments.
 		/// </summary>
 		/// <param name="func">Function to memoize.</param>
+		/// <param name="threadSafe">If true - returns thread safe implementation</param>
 		/// <typeparam name="TArg1">Type of argument 1</typeparam>
 		/// <typeparam name="TArg2">Type of argument 2</typeparam>
 		/// <typeparam name="TArg3">Type of argument 3</typeparam>
@@ -84,15 +102,21 @@ namespace CodeJam.Threading
 		[NotNull]
 		[Pure]
 		public static Func<TArg1, TArg2, TArg3, TArg4, TResult> Memoize<TArg1, TArg2, TArg3, TArg4, TResult>(
-			[NotNull] this Func<TArg1, TArg2, TArg3, TArg4, TResult> func) =>
-				(arg1, arg2, arg3, arg4) => new ConcurrentLazyDictionary<Tuple<TArg1, TArg2, TArg3, TArg4>, TResult>(
-					key => func(key.Item1, key.Item2, key.Item3, key.Item4))
-						.Get(Tuple.Create(arg1, arg2, arg3, arg4));
+			[NotNull] this Func<TArg1, TArg2, TArg3, TArg4, TResult> func,
+			bool threadSafe = false)
+		{
+			var map =
+				LazyDictionary.Create<TupleStruct<TArg1, TArg2, TArg3, TArg4>, TResult>(
+					key => func(key.Item1, key.Item2, key.Item3, key.Item4),
+					threadSafe);
+			return (arg1, arg2, arg3, arg4) => map.Get(TupleStruct.Create(arg1, arg2, arg3, arg4));
+		}
 
 		/// <summary>
 		/// Caches function value for specific arguments.
 		/// </summary>
 		/// <param name="func">Function to memoize.</param>
+		/// <param name="threadSafe">If true - returns thread safe implementation</param>
 		/// <typeparam name="TArg1">Type of argument 1</typeparam>
 		/// <typeparam name="TArg2">Type of argument 2</typeparam>
 		/// <typeparam name="TArg3">Type of argument 3</typeparam>
@@ -103,10 +127,15 @@ namespace CodeJam.Threading
 		[NotNull]
 		[Pure]
 		public static Func<TArg1, TArg2, TArg3, TArg4, TArg5, TResult> Memoize<TArg1, TArg2, TArg3, TArg4, TArg5, TResult>(
-			[NotNull] this Func<TArg1, TArg2, TArg3, TArg4, TArg5, TResult> func) =>
-				(arg1, arg2, arg3, arg4, arg5) => new ConcurrentLazyDictionary<Tuple<TArg1, TArg2, TArg3, TArg4, TArg5>, TResult>(
-					key => func(key.Item1, key.Item2, key.Item3, key.Item4, key.Item5))
-						.Get(Tuple.Create(arg1, arg2, arg3, arg4, arg5));
+			[NotNull] this Func<TArg1, TArg2, TArg3, TArg4, TArg5, TResult> func,
+			bool threadSafe = false)
+		{
+			var map =
+				LazyDictionary.Create<TupleStruct<TArg1, TArg2, TArg3, TArg4, TArg5>, TResult>(
+					key => func(key.Item1, key.Item2, key.Item3, key.Item4, key.Item5),
+					threadSafe);
+			return (arg1, arg2, arg3, arg4, arg5) => map.Get(TupleStruct.Create(arg1, arg2, arg3, arg4, arg5));
+		}
 		#endregion
 	}
 }
--- Main/tests/Threading/ThreadingExtensionsTests.cs
+++ Main/tests/Threading/ThreadingExtensionsTests.cs
@@ -7,17 +7,33 @@ namespace CodeJam.Threading
 	[TestFixture]
 	public class ThreadingExtensionsTests
 	{
-		[Test]
-		public void Memoize()
+		[TestCase(true)]
+		[TestCase(false)]
+		public void Memoize(bool threadSafe)
 		{
 			var calls = 0;
 			Func<string, int> func = str => { calls++; return str.Length; };
-			var memFunc = func.Memoize();
+			var memFunc = func.Memoize(threadSafe);
 			Assert.AreEqual(7, memFunc("O La La"), "#A01");
 			Assert.AreEqual(1, calls, "#A02");
 			Assert.AreEqual(7, memFunc("O La La"), "#A03");
 			Assert.AreEqual(1, calls, "#A04");
 			Assert.AreEqual(9, memFunc("O La La 2"), "#A05");
+			Assert.AreEqual(2, calls, "#A06");
+		}
+
+		[TestCase(true)]
+		[TestCase(false)]
+		public void MemoizeTuple(bool threadSafe)
+		{
+			var calls = 0;
+			Func<string, string, int> func = (s1, s2) => { calls++; return s1.Length + s2.Length; };
+			var memFunc = func.Memoize(threadSafe);
+			Assert.AreEqual(15, memFunc("O La La", "Ho Ho Ho"), "#A01");
+			Assert.AreEqual(1, calls, "#A02");
+			Assert.AreEqual(15, memFunc("O La La", "Ho Ho Ho"), "#A03");
+			Assert.AreEqual(1, calls, "#A04");
+			Assert.AreEqual(19, memFunc("O La La 2", "Ho Ho Ho 2"), "#A05");
 			Assert.AreEqual(2, calls, "#A06");
 		}
 	}
