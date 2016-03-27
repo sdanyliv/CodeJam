60e1f21b415fb5a7e57e8ea00320a97f3c1e93de
andrewvk <avk@rsdn.ru>
2016-03-27 16:11:57 +0300

    GetTempDir

--- Main/src/IO/TempHelper.cs
+++ Main/src/IO/TempHelper.cs
@@ -14,22 +14,45 @@ namespace CodeJam.IO
 		/// <summary>
 		/// Creates temp directory and returns <see cref="IDisposable"/> to free it.
 		/// </summary>
-		public static Disposable.AnonymousDisposable GetTempDir()
+		public static TempDir GetTempDir()
 		{
 			var dirName= new Guid() + ".tmp";
 			var dirPath = Path.Combine(Path.GetTempPath(), dirName);
 			Directory.CreateDirectory(dirPath);
-			return
-				Disposable.Create(
-					() =>
-					{
-						try
-						{
-							Directory.Delete(dirPath, true);
-						}
-						// ReSharper disable once EmptyGeneralCatchClause
-						catch {}
-					});
+			return new TempDir(dirPath);
+		}
+
+		/// <summary>
+		/// Temp directory.
+		/// </summary>
+		[PublicAPI]
+		public struct TempDir : IDisposable
+		{
+			/// <summary>
+			/// Initialize instance.
+			/// </summary>
+			public TempDir(string path)
+			{
+				Path = path;
+			}
+
+			/// <summary>
+			/// Path to directory.
+			/// </summary>
+			public string Path { get; }
+
+			/// <summary>
+			/// Delete directory and all its content.
+			/// </summary>
+			public void Dispose()
+			{
+				try
+				{
+					Directory.Delete(Path, true);
+				}
+				// ReSharper disable once EmptyGeneralCatchClause
+				catch { }
+			}
 		}
 	}
 }
---
+++ Main/tests/IO/TempHelperTests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+using NUnit.Framework;
+
+namespace CodeJam.IO
+{
+	[TestFixture]
+	public class TempHelperTests
+	{
+		[Test]
+		public void TempDir()
+		{
+			string dirPath;
+			using (var dir = TempHelper.GetTempDir())
+			{
+				dirPath = dir.Path;
+				Assert.IsTrue(Directory.Exists(dirPath), "#A01");
+			}
+			Assert.IsFalse(Directory.Exists(dirPath), "#A02");
+
+			using (var dir = TempHelper.GetTempDir())
+			{
+				dirPath = dir.Path;
+				File.WriteAllText(Path.Combine(dirPath, "test.tmp"), "O La La");
+			}
+			Assert.IsFalse(Directory.Exists(dirPath), "#A03");
+
+			using (var dir = TempHelper.GetTempDir())
+			{
+				dirPath = dir.Path;
+				var subDir = Path.Combine(dirPath, "Subdir");
+				Directory.CreateDirectory(subDir);
+				File.WriteAllText(Path.Combine(subDir, "test.tmp"), "O La La");
+			}
+			Assert.IsFalse(Directory.Exists(dirPath), "#A04");
+		}
+	}
+}
