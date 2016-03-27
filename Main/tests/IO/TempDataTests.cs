4e4cf8d938a910e2f53898ee93db3e141b277d59
andrewvk <avk@rsdn.ru>
2016-03-28 00:59:15 +0300

    Temp^ GetFile, GetStream.

--- Main/tests/IO/TempDataTests.cs
+++ Main/tests/IO/TempDataTests.cs
@@ -5,27 +5,27 @@ using NUnit.Framework;
 namespace CodeJam.IO
 {
 	[TestFixture]
-	public class TempHelperTests
+	public class TempDataTests
 	{
 		[Test]
-		public void TempDir()
+		public void Dir()
 		{
 			string dirPath;
-			using (var dir = TempHelper.GetTempDir())
+			using (var dir = TempData.GetDir())
 			{
 				dirPath = dir.Path;
 				Assert.IsTrue(Directory.Exists(dirPath), "#A01");
 			}
 			Assert.IsFalse(Directory.Exists(dirPath), "#A02");
 
-			using (var dir = TempHelper.GetTempDir())
+			using (var dir = TempData.GetDir())
 			{
 				dirPath = dir.Path;
 				File.WriteAllText(Path.Combine(dirPath, "test.tmp"), "O La La");
 			}
 			Assert.IsFalse(Directory.Exists(dirPath), "#A03");
 
-			using (var dir = TempHelper.GetTempDir())
+			using (var dir = TempData.GetDir())
 			{
 				dirPath = dir.Path;
 				var subDir = Path.Combine(dirPath, "Subdir");
@@ -33,6 +33,31 @@ namespace CodeJam.IO
 				File.WriteAllText(Path.Combine(subDir, "test.tmp"), "O La La");
 			}
 			Assert.IsFalse(Directory.Exists(dirPath), "#A04");
+		}
+
+		[Test]
+		public void TmpFile()
+		{
+			string path;
+			using (var fl = TempData.GetFile())
+			{
+				path = fl.Path;
+				Assert.IsTrue(File.Exists(path), "#A01");
+			}
+			Assert.IsFalse(File.Exists(path), "#A02");
+		}
+
+		[Test]
+		public void Stream()
+		{
+			string path;
+			using (var stream = TempData.GetStream())
+			{
+				new StreamWriter(stream.Stream).Write("O La La");
+				path = stream.Path;
+				Assert.IsTrue(File.Exists(path), "#A01");
+			}
+			Assert.IsFalse(File.Exists(path), "#A02");
 		}
 	}
 }
