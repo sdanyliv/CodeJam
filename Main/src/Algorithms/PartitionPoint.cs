e29703b35fa00dd6fa98a92bf6b63ade391ac7ea
Lexey <lexeyo@gmail.com>
2016-03-28 20:26:29 +0300

    Added PartitionPoint algorithm

---
+++ Main/src/Algorithms/PartitionPoint.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeJam
+{
+	partial class Algorithms
+	{
+		/// <summary>
+		/// Returns the index i in the range [0, list.Count - 1] such that
+		///		predicate(list[j]) = true for j &lt; i
+		///		and predicate(list[k]) = false for k >= i
+		/// or list.Count if no such i exists
+		/// <remarks>The list should be partitioned according to the predicate</remarks>
+		/// </summary>
+		/// <typeparam name="T">The list element type</typeparam>
+		/// <param name="list">The sorted list</param>
+		/// <param name="predicate">The predicate</param>
+		/// <returns>The partition point</returns>
+		public static int PartitionPoint<T>(this IList<T> list, Predicate<T> predicate)
+			=> PartitionPoint(list, 0, list.Count, predicate);
+
+		/// <summary>
+		/// Returns the index i in the range [from, list.Count - 1] such that
+		///		predicate(list[j]) = true for j &lt; i
+		///		and predicate(list[k]) = false for k >= i
+		/// or list.Count if no such i exists
+		/// <remarks>The list should be partitioned according to the predicate</remarks>
+		/// </summary>
+		/// <typeparam name="T">The list element type</typeparam>
+		/// <param name="list">The sorted list</param>
+		/// <param name="from">The minimum index</param>
+		/// <param name="predicate">The predicate</param>
+		/// <returns>The partition point</returns>
+		public static int PartitionPoint<T>(this IList<T> list, int from, Predicate<T> predicate)
+			=> PartitionPoint(list, from, list.Count, predicate);
+
+		/// <summary>
+		/// Returns the index i in the range [from, to - 1] such that
+		///		predicate(list[j]) = true for j &lt; i
+		///		and predicate(list[k]) = false for k >= i
+		/// or "to" if no such i exists
+		/// <remarks>The list should be partitioned according to the predicate</remarks>
+		/// </summary>
+		/// <typeparam name="T">The list element type</typeparam>
+		/// <param name="list">The sorted list</param>
+		/// <param name="from">The minimum index</param>
+		/// <param name="to">The upper bound for the index (not included)</param>
+		/// <param name="predicate">The predicate</param>
+		/// <returns>The partition point</returns>
+		public static int PartitionPoint<T>(this IList<T> list, int from, int to, Predicate<T> predicate)
+		{
+			ValidateIndicesRange(from, to, list.Count);
+			while (from < to)
+			{
+				var median = from + (to - from) / 2;
+				var testResult = predicate(list[median]);
+				if (!testResult)
+				{
+					to = median;
+				}
+				else
+				{
+					from = median + 1;
+				}
+			}
+			return from;
+		}
+	}
+}
---
+++ Main/tests/Algorithms/PartitionPointTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace CodeJam
+{
+	[TestFixture]
+	public class PartitionPointTest
+	{
+		[Test]
+		public void Test01NegativeFrom()
+		{
+			var list = new List<int> { 0 };
+			const int from = -1;
+			Assert.That(() => list.PartitionPoint(from, list.Count, _ => _ > 0)
+				, Throws.InstanceOf(typeof(ArgumentOutOfRangeException)));
+		}
+
+		[Test]
+		public void Test02NegativeTo()
+		{
+			var list = new List<int> { 0 };
+			const int to = -1;
+			Assert.That(() => list.PartitionPoint(0, to, _ => _ > 0)
+				, Throws.InstanceOf(typeof(ArgumentOutOfRangeException)));
+		}
+
+
+		[Test]
+		public void Test03ToExceedsCount()
+		{
+			var list = new List<int> { 0 };
+			var to = list.Count + 1;
+			Assert.That(() => list.PartitionPoint(0, to, _ => _ > 0)
+				, Throws.InstanceOf(typeof(ArgumentOutOfRangeException)));
+		}
+
+		[Test]
+		public void Test04BadFromToOrder()
+		{
+			var list = new List<int> { 0, 1, 2 };
+			const int from = 2;
+			const int to = 0;
+			Assert.That(() => list.PartitionPoint(from, to, _ => _ > 0), Throws.ArgumentException);
+		}
+
+		[Test]
+		[TestCase(new int[0], 1, 0, 0, ExpectedResult = 0)]
+		[TestCase(new[] { 1, 5, 12, 35, 123 }, 10, 0, 0, ExpectedResult = 0)]
+		[TestCase(new[] { 1, 5, 12, 35, 123 }, 11, 2, 2, ExpectedResult = 2)]
+		[TestCase(new[] { 1, 5, 12, 35, 123 }, 30, 5, 5, ExpectedResult = 5)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 15, 0, 3, ExpectedResult = 3)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 5, 1, 4, ExpectedResult = 1)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 30000, 0, 4, ExpectedResult = 4)]
+		public int Test05WithAllParams(int[] data, int partitionValue, int from, int to)
+		{
+			var list = (IList<int>)data;
+			return list.PartitionPoint(from, to, _ => _ < partitionValue);
+		}
+
+		[Test]
+		[TestCase(new int[0], 4, 0, ExpectedResult = 0)]
+		[TestCase(new[] { 1, 5, 12, 35, 123 }, 35, 5, ExpectedResult = 5)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 15, 0, ExpectedResult = 4)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 5, 0, ExpectedResult = 1)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, -1, 4, ExpectedResult = 4)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 42, 6, ExpectedResult = 6)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 1002, 3, ExpectedResult = 7)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 12, 3, ExpectedResult = 3)]
+		public int Test06WithFrom(int[] data, int partitionValue, int from)
+		{
+			var list = (IList<int>)data;
+			return list.PartitionPoint(from, _ => _ < partitionValue);
+		}
+
+		[Test]
+		[TestCase(new int[0], 14, ExpectedResult = 0)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 50, ExpectedResult = 4)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 5, ExpectedResult = 1)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 30000, ExpectedResult = 8)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, -1, ExpectedResult = 0)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 42, ExpectedResult = 4)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 1002, ExpectedResult = 7)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 12, ExpectedResult = 2)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 3, ExpectedResult = 1)]
+		[TestCase(new[] { 1, 5, 12, 12, 123, 512, 512, 14534 }, 14534, ExpectedResult = 7)]
+		public int Test07WithoutParams(int[] data, int partitionValue)
+		{
+			var list = (IList<int>)data;
+			return list.PartitionPoint(_ => _ < partitionValue);
+		}
+	}
+}
