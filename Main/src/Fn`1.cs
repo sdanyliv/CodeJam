34619722b0d11f344b339abd6fc9a59a45b55444
rameel <rameel-b@hotmail.com>
2016-03-29 22:55:56 +0600

    Fixed comments

--- Main/src/Fn`1.cs
+++ Main/src/Fn`1.cs
@@ -8,37 +8,37 @@ namespace CodeJam
 	public static class Fn<T>
 	{
 		/// <summary>
-		/// Gets the function that always returns true.
+		/// The function that always returns true.
 		/// </summary>
 		public static readonly Func<T, bool> True = o => true;
 
 		/// <summary>
-		/// Gets the function that returns false.
+		/// The function that returns false.
 		/// </summary>
 		public static readonly Func<T, bool> False = o => false;
 
 		/// <summary>
-		/// Gets the function that returns true.
+		/// The function that returns true.
 		/// </summary>
 		public static readonly Predicate<T> TruePredicate = o => true;
 
 		/// <summary>
-		/// Gets the function that always returns false.
+		/// The function that always returns false.
 		/// </summary>
 		public static readonly Predicate<T> FalsePredicate = o => false;
 
 		/// <summary>
-		/// Gets the function that returns the same object which was passed as parameter.
+		/// The function that returns the same object which was passed as parameter.
 		/// </summary>
 		public static readonly Func<T, T> Identity = o => o;
 
 		/// <summary>
-		/// Gets the function that returns the same object which was passed as parameter.
+		/// The function that returns the same object which was passed as parameter.
 		/// </summary>
 		public static readonly Converter<T, T> IdentityConverter = o => o;
 
 		/// <summary>
-		/// Gets the function that returns true if an object is not null.
+		/// The function that returns true if an object is not null.
 		/// </summary>
 		public static readonly Func<T, bool> IsNotNull = o => o != null;
 	}
