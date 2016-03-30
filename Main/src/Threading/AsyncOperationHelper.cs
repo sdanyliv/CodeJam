f752ad39d5e6d022414aa33e0e822cff95f23b25
andrewvk <avk@rsdn.ru>
2016-03-30 22:42:08 +0300

    AsyncOperationHelper

---
+++ Main/src/Threading/AsyncOperationHelper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.Threading
+{
+	/// <summary>
+	/// Extension and utility methods for <see cref="AsyncOperationManager"/> ans <see cref="AsyncOperation"/>
+	/// </summary>
+	[PublicAPI]
+	public static class AsyncOperationHelper
+	{
+		/// <summary>
+		/// Returns an <see cref="AsyncOperation"/> for tracking the duration of a particular asynchronous operation.
+		/// </summary>
+		/// <returns>
+		/// An <see cref="AsyncOperation"/> that you can use to track the duration of an asynchronous method invocation.
+		/// </returns>
+		[NotNull]
+		[Pure]
+		public static AsyncOperation CreateOperation() => AsyncOperationManager.CreateOperation(null);
+
+		/// <summary>
+		/// Invokes a <paramref name="runner"/> on the thread or context appropriate for the application model.
+		/// </summary>
+		/// <param name="asyncOp"></param>
+		/// <param name="runner">
+		/// A <see cref="Action"/> that wraps the delegate to be called when the operation ends.
+		/// </param>
+		public static void Post([NotNull] this AsyncOperation asyncOp, [NotNull, InstantHandle] Action runner)
+		{
+			if (asyncOp == null) throw new ArgumentNullException(nameof(asyncOp));
+			if (runner == null) throw new ArgumentNullException(nameof(runner));
+			asyncOp.Post(state => runner(), null);
+		}
+
+		/// <summary>
+		/// Ends the lifetime of an asynchronous operation.
+		/// </summary>
+		/// <param name="asyncOp"></param>
+		/// <param name="runner">A <see cref="Action"/> that wraps the delegate to be called when the operation ends.</param>
+		public static void PostOperationCompleted(
+			[NotNull] this AsyncOperation asyncOp,
+			[NotNull, InstantHandle] Action runner)
+		{
+			if (asyncOp == null) throw new ArgumentNullException(nameof(asyncOp));
+			if (runner == null) throw new ArgumentNullException(nameof(runner));
+			asyncOp.PostOperationCompleted(state => runner(), null);
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="runner"/> on the thread or context appropriate for the application model and waits for
+		/// it completion.
+		/// </summary>
+		/// <param name="asyncOp"></param>
+		/// <param name="runner">
+		/// A <see cref="Action"/> that wraps the delegate to be called when the operation ends.
+		/// </param>
+		public static void Send([NotNull] this AsyncOperation asyncOp, [NotNull] Action runner)
+		{
+			if (asyncOp == null) throw new ArgumentNullException(nameof(asyncOp));
+			if (runner == null) throw new ArgumentNullException(nameof(runner));
+
+			asyncOp.SynchronizationContext.Send(state => runner(), null);
+		}
+
+		/// <summary>
+		/// Invokes a <paramref name="runner"/> on the thread or context appropriate for the application model and returns
+		/// result.
+		/// </summary>
+		/// <param name="asyncOp"></param>
+		/// <param name="runner">
+		/// A <see cref="Func{TResult}"/> that wraps the delegate to be called when the operation ends.
+		/// </param>
+		public static T Send<T>([NotNull] this AsyncOperation asyncOp, [NotNull] Func<T> runner)
+		{
+			if (asyncOp == null) throw new ArgumentNullException(nameof(asyncOp));
+			if (runner == null) throw new ArgumentNullException(nameof(runner));
+			var result = default(T);
+			asyncOp.SynchronizationContext.Send(state => result = runner(), null);
+			return result;
+		}
+
+		/// <summary>
+		/// Gets thread from pool and run <paramref name="runner"/> inside it.
+		/// </summary>
+		/// <param name="runner">Action to run inside created thread</param>
+		public static void RunAsync([NotNull] Action<AsyncOperation> runner)
+		{
+			if (runner == null)
+				throw new ArgumentNullException(nameof(runner));
+
+			var asyncOp = CreateOperation();
+			ThreadPool.QueueUserWorkItem(state => runner(asyncOp));
+		}
+
+		/// <summary>
+		/// Gets thread from pool and run <paramref name="runner"/> inside it.
+		/// </summary>
+		/// <param name="runner">Action to run inside created thread</param>
+		/// <param name="completeHandler">
+		/// Action called after <paramref name="runner"/> complete execution. Synchronized with method calling thread.
+		/// </param>
+		public static void RunAsync(
+			Action<AsyncOperation> runner,
+			Action completeHandler)
+		{
+			if (runner == null)
+				throw new ArgumentNullException(nameof(runner));
+			if (completeHandler == null)
+				throw new ArgumentNullException(nameof(completeHandler));
+
+			var asyncOp = CreateOperation();
+			ThreadPool.QueueUserWorkItem(
+				state =>
+				{
+					try
+					{
+						runner(asyncOp);
+					}
+					finally
+					{
+						asyncOp.PostOperationCompleted(completeHandler);
+					}
+				});
+		}
+	}
+}
