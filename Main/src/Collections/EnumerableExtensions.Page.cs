e2bc3c9cb74cb88244e20e9d4aea9c11d2bda2c3
rameel <rameel-b@hotmail.com>
2016-04-01 22:00:42 +0600

    Added EnumerableExtensions.Page

---
+++ Main/src/Collections/EnumerableExtensions.Page.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.Collections
+{
+	partial class EnumerableExtensions
+	{
+		/// <summary>
+		/// Extracts <paramref name="pageSize"/> elements from a sequence at a particular one-based page number.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+		/// <param name="source">The sequence from which to page.</param>
+		/// <param name="pageIndex">The one-based page number.</param>
+		/// <param name="pageSize">The size of the page.</param>
+		/// <returns>
+		/// A new sequence containing elements are at the specified <paramref name="pageIndex"/> from the source sequence.
+		/// </returns>
+		[NotNull, Pure]
+		public static IEnumerable<T> Page<T>([NotNull] this IEnumerable<T> source, int pageIndex, int pageSize) =>
+			source.Slice((pageIndex - 1)*pageSize, pageSize);
+	}
+}
---
+++ Main/tests/Collections/EnumerableExtensionTests.Page.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+using CodeJam.Collections;
+
+using NUnit.Framework;
+
+namespace CodeJam
+{
+	partial class EnumerableExtensionTests
+	{
+		[TestCase(0, ExpectedResult = "")]
+		[TestCase(1, ExpectedResult = "")]
+		[TestCase(2, ExpectedResult = "")]
+		[TestCase(3, ExpectedResult = "2")]
+		[TestCase(4, ExpectedResult = "2, 3")]
+		[TestCase(5, ExpectedResult = "2, 3")]
+		public string Page1Test(int n)
+		{
+			var source = n > 0
+				? Enumerable.Range(0, n)
+				: Enumerable.Empty<int>();
+			return source.Page(2, 2).Join(", ");
+		}
+
+		[TestCase(0, ExpectedResult = "")]
+		[TestCase(1, ExpectedResult = "0")]
+		[TestCase(2, ExpectedResult = "0, 1")]
+		[TestCase(3, ExpectedResult = "0, 1")]
+		[TestCase(4, ExpectedResult = "0, 1")]
+		[TestCase(5, ExpectedResult = "0, 1")]
+		public string Page2Test(int n)
+		{
+			var source = n > 0
+				? Enumerable.Range(0, n)
+				: Enumerable.Empty<int>();
+			return source.Page(1, 2).Join(", ");
+		}
+
+		[Test]
+		public void Page3Test()
+		{
+			for (var n = 1; n < 100; n++)
+			{
+				var actual = Enumerable.Range(0, n).Page(1, n);
+				var expected = Enumerable.Range(0, n);
+				Assert.That(actual, Is.EquivalentTo(expected));
+			}
+		}
+
+		[TestCase(new[] {3, 1, 8, 0, 6}, ExpectedResult = true)]
+		[TestCase(new[] {3, 1, 8},       ExpectedResult = true)]
+		[TestCase(new[] {3, 1},          ExpectedResult = true)]
+		[TestCase(new[] {1},             ExpectedResult = true)]
+		public bool PageIdentityTest(int[] source) =>
+			// Page from 0 to sequence.Count returns the same sequence
+			ReferenceEquals(source.Page(1, source.Length), source);
+	}
+}
