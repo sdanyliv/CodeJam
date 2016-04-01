b417e286a0e76267ba9274130551d918cb8fcaad
rameel <rameel-b@hotmail.com>
2016-04-01 22:49:49 +0600

    Added EnumerableExtensions.Index

---
+++ Main/src/Collections/EnumerableExtensions.Index.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.Collections
+{
+	partial class EnumerableExtensions
+	{
+		/// <summary>
+		/// Associates an index to each element of the source sequence.
+		/// </summary>
+		/// <param name="source">The input sequence.</param>
+		/// <returns>
+		/// A sequence of elements paired with their index in the sequence.
+		/// </returns>
+		[NotNull, Pure]
+		public static IEnumerable<IndexedItem<T>> Index<T>([NotNull] this IEnumerable<T> source)
+		{
+			Code.NotNull(source, nameof(source));
+			return IndexImpl(source);
+		}
+
+		private static IEnumerable<IndexedItem<T>> IndexImpl<T>(IEnumerable<T> source)
+		{
+			using (var enumerator = source.GetEnumerator())
+			{
+				if (enumerator.MoveNext())
+				{
+					var index = 0;
+					var isFirst = true;
+					var isLast = false;
+
+					while (!isLast)
+					{
+						var item = enumerator.Current;
+						isLast = !enumerator.MoveNext();
+
+						yield return new IndexedItem<T>(item, index++, isFirst, isLast);
+						isFirst = false;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Main/src/Collections/IndexedItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CodeJam.Collections 
+{
+	/// <summary>
+	/// Represents an element associated with its index in a sequence.
+	/// </summary>
+	public struct IndexedItem<T>
+	{
+		/// <summary>
+		/// Gets the value of the element.
+		/// </summary>
+		/// <returns>
+		/// The value of the element.
+		/// </returns>
+		public T Item { get; }
+
+		/// <summary>
+		/// Gets the index of the element in a sequence.
+		/// </summary>
+		/// <returns>
+		/// The index of the element in a sequence.
+		/// </returns>
+		public int Index { get; }
+
+		/// <summary>
+		/// Determines if the value is first in a sequence.
+		/// </summary>
+		/// <returns>
+		/// <c>true</c> if this instance is first; otherwise, <c>false</c>.
+		/// </returns>
+		public bool IsFirst { get; }
+
+		/// <summary>
+		/// Determines if the value is last in a sequence.
+		/// </summary>
+		/// <returns>
+		/// <c>true</c> if this instance is last; otherwise, <c>false</c>.
+		/// </returns>
+		public bool IsLast { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="IndexedItem{T}"/>.
+		/// </summary>
+		/// <param name="item">The value of the element.</param>
+		/// <param name="index">The index of the element in a sequence.</param>
+		/// <param name="isFirst">A value indicating whether this instance is first.</param>
+		/// <param name="isLast">A value indicating whether this instance is last.</param>
+		public IndexedItem(T item, int index, bool isFirst, bool isLast) : this()
+		{
+			Index = index;
+			Item = item;
+			IsFirst = isFirst;
+			IsLast = isLast;
+		}
+	}
+}
