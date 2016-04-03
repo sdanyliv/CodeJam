3eddf5321eeb93d2bb52af919f077d6ea4d95b8a
rameel <rameel-b@hotmail.com>
2016-04-04 00:37:05 +0600

    Updated ExpressionHelper and InfoOf classes

--- Main/tests/Reflection/InfoOfTest.cs
+++ Main/tests/Reflection/InfoOfTest.cs
@@ -9,12 +9,24 @@ namespace CodeJam.Reflection
 	public class InfoOfTest
 	{
 		[Test]
-		public void ExtarctingPropertyInfo()
+		public void ExtarctingPropertyInfo1()
 		{
-			var expected = typeof (User).GetProperty(nameof(User.Name));
+			var expected = typeof (User).GetProperty(nameof (User.Name));
 
 			var info1 = InfoOf.Property(() => new User().Name);
-			var info2 = InfoOf<User>.Property(u => u.Name);
+			var info2 = InfoOf.Property<User>(u => u.Name);
+
+			Assert.AreEqual(expected, info1, "#1");
+			Assert.AreEqual(expected, info2, "#2");
+		}
+
+		[Test]
+		public void ExtarctingPropertyInfo2()
+		{
+			var expected = typeof (User).GetProperty(nameof (User.Age));
+
+			var info1 = InfoOf.Property(() => new User().Age);
+			var info2 = InfoOf.Property<User>(u => u.Age);
 
 			Assert.AreEqual(expected, info1, "#1");
 			Assert.AreEqual(expected, info2, "#2");
@@ -23,10 +35,10 @@ namespace CodeJam.Reflection
 		[Test]
 		public void ExtractingFieldInfo()
 		{
-			var expected = typeof (User).GetField(nameof(User.LastName));
+			var expected = typeof (User).GetField(nameof (User.LastName));
 
 			var info1 = InfoOf.Field(() => new User().LastName);
-			var info2 = InfoOf<User>.Field(u => u.LastName);
+			var info2 = InfoOf.Field<User>(u => u.LastName);
 
 			Assert.AreEqual(expected, info1, "#1");
 			Assert.AreEqual(expected, info2, "#2");
@@ -37,7 +49,7 @@ namespace CodeJam.Reflection
 		{
 			var expected = typeof (User).GetConstructors().First(c => c.GetParameters().Length != 0);
 			var ctor1 = InfoOf.Constructor(() => new User("", ""));
-			var ctor2 = InfoOf<User>.Constructor(() => new User("", ""));
+			var ctor2 = InfoOf.Constructor<User>(u => new User("", ""));
 
 			Assert.AreEqual(expected, ctor1, "#1");
 			Assert.AreEqual(expected, ctor2, "#2");
@@ -48,17 +60,29 @@ namespace CodeJam.Reflection
 		{
 			var expected = typeof (User).GetMethod("Debug");
 			var method1 = InfoOf.Method(() => new User().Debug());
-			var method2 = InfoOf<User>.Method(u => u.Debug());
+			var method2 = InfoOf.Method<User>(u => u.Debug());
+
+			Assert.AreEqual(expected, method1, "#1");
+			Assert.AreEqual(expected, method2, "#2");
+		}
+
+		[Test]
+		public void ExtarctingPropertyGetMethod()
+		{
+			var expected = typeof (User).GetProperty("Name").GetMethod;
+			var method1 = InfoOf.Method(() => new User().Name);
+			var method2 = InfoOf.Method<User>(u => u.Name);
 
 			Assert.AreEqual(expected, method1, "#1");
 			Assert.AreEqual(expected, method2, "#2");
 		}
 
 		#region Inner types
-		private class User
+		public class User
 		{
-			public string Name { get; }
-			public readonly string LastName;
+			public string Name { get; set; }
+			public string LastName;
+			public int Age { get; set; }
 
 			public User()
 			{
@@ -70,11 +94,10 @@ namespace CodeJam.Reflection
 				LastName = lastName;
 			}
 
-			// ReSharper disable once MemberCanBeMadeStatic.Local
 			public void Debug()
-			{}
+			{
+			}
 		}
-
 		#endregion
 	}
 }
